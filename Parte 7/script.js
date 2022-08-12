3848dd7dc451fe6b3b6fa08935da8995125d4d55
JustinRT1 <itsjash11@gmail.com>
2022-08-11 22:10:29 -0500

    [UPDATE] Project 7:Side-scroll Game - Terminado

--- Parte 7/script.js
+++ Parte 7/script.js
@@ -71,10 +71,10 @@ window.addEventListener('load', function () {
         update(input, deltaTime, enemies) {
             // Collision detection
             enemies.forEach(enemy => {
-                const dx = (enemy.x + enemy.width / 2) - (this.x + this.width / 2)
-                const dy = (enemy.y + enemy.height / 2) - (this.y + this.height / 2)
+                const dx = (enemy.x + enemy.width / 2 - 20) - (this.x + this.width / 2)
+                const dy = (enemy.y + enemy.height / 2) - (this.y + this.height / 2 + 20)
                 const distance = Math.sqrt(dx * dx + dy * dy)
-                if (distance < enemy.width / 2 + this.width / 2) {
+                if (distance < enemy.width / 3 + this.width / 3) {
                     gameOver = true
                 }
             })
@@ -115,14 +115,10 @@ window.addEventListener('load', function () {
         }
         draw(context) {
             // * Collision Architecture 
+            // context.lineWidth = 5
             // context.strokeStyle = 'white'
-            // context.strokeRect(this.x, this.y, this.width, this.height)
             // context.beginPath()
-            // context.arc(this.x + this.width / 2, this.y + this.height / 2, this.width / 2, 0, Math.PI * 2)
-            // context.stroke()
-            // context.strokeStyle = 'blue'
-            // context.beginPath()
-            // context.arc(this.x, this.y, this.width / 2, 0, Math.PI * 2)
+            // context.arc(this.x + this.width / 2, this.y + this.height / 2 + 20, this.width / 3, 0, Math.PI * 2)
             // context.stroke()
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
         }
@@ -194,14 +190,10 @@ window.addEventListener('load', function () {
         }
         draw(context) {
             // * Collision Architecture
+            // context.lineWidth = 5
             // context.strokeStyle = 'white'
-            // context.strokeRect(this.x, this.y, this.width, this.height)
             // context.beginPath()
-            // context.arc(this.x + this.width / 2, this.y + this.height / 2, this.width / 2, 0, Math.PI * 2)
-            // context.stroke()
-            // context.strokeStyle = 'blue'
-            // context.beginPath()
-            // context.arc(this.x, this.y, this.width / 2, 0, Math.PI * 2)
+            // context.arc(this.x + this.width / 2 - 20, this.y + this.height / 2 + 20, this.width / 3, 0, Math.PI * 2)
             // context.stroke()
             context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
         }
