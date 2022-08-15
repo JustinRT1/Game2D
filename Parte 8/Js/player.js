5778305dd61b0aa3e3ca366aec76cb3e2af09255
JustinRT1 <itsjash11@gmail.com>
2022-08-14 21:24:38 -0500

    [UPDATE] State management - States

--- Parte 8/Js/player.js
+++ Parte 8/Js/player.js
@@ -1,21 +1,33 @@
+import { StandingLeft, StandingRight, SittingLeft, SittingRight, RunningLeft, RunningRight } from "./state.js"
+
 export default class Player {
     constructor(gameWidth, gameHeight) {
         this.gameWidht = gameWidth
         this.gameHeight = gameHeight
-        this.states = []
-        this.currentState = this.states[0]
+        this.states = [new StandingLeft(this), new StandingRight(this), new SittingLeft(this), new SittingRight(this), new RunningLeft(this), new RunningRight(this)]
+        this.currentState = this.states[1]
         this.image = dogImage
         this.width = 200
         this.height = 181.83
-        this.x = this.gameWidht/2 - this.width/2
+        this.x = this.gameWidht / 2 - this.width / 2
         this.y = this.gameHeight - this.height
         this.frameX = 0
         this.frameY = 0
+        this.speed = 0
+        this.maxSpeed = 10
     }
-    update() {
-
+    update(input) {
+        this.currentState.handleInput(input)
+        // Horizontal movement
+        this.x += this.speed
+        if (this.x <= 0) this.x = 0
+        else if (this.x >= this.gameWidht - this.width) this.x = this.gameWidht - this.width
     }
     draw(context) {
         context.drawImage(this.image, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+    setState(state) {
+        this.currentState = this.states[state]
+        this.currentState.enter()
     }
 }
